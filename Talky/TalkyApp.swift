f59e517234806b0d4f056de1d887ce8b02328cae
Muhammad Rezky <mrezkysulihin@gmail.com>
2023-06-24 20:30:05 +0700

    Initial Commit

---
+++ Talky/TalkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkyApp.swift
+//  Talky
+//
+//  Created by Muhammad Rezky on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TalkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
